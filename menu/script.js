2ca42af6826c13d7b8b03bbf787d51d14a22491b
Maxim Melkonian <mmelk057@uottawa.ca>
2019-05-31 10:20:47 -0400

    #7: Add and Remove Items from Order

--- menu/script.js
+++ menu/script.js
@@ -2,6 +2,30 @@ function changePage(){
     location.replace("http://127.0.0.1:5500/order/OrderList.html")
 }
 
+function increment(foodType){
+    let foodName;
+    if (foodType==='ribs'){
+        foodName = "ribs";
+    } else {
+        foodName = "burger";
+    }
+    var currentQuantity = parseInt(document.querySelector("#"+foodName).firstChild.data); 
+    document.querySelector("#"+foodName).firstChild.data = currentQuantity+1;
+}
+
+function decrement(foodType){
+    let foodName;
+    if (foodType==='ribs'){
+        foodName = "ribs";
+    } else {
+        foodName = "burger";
+    }
+    var currentQuantity = parseInt(document.querySelector("#"+foodName).firstChild.data);
+    if(currentQuantity > 0){
+        document.querySelector("#"+foodName).firstChild.data = currentQuantity-1;
+    }
+}
+
 function Home(){
     location.replace("http://127.0.0.1:5500/welcome/")
 }
