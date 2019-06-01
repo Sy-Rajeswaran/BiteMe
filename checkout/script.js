ee0a4a8dffef8487b42f8c39cfe2e9d0b11fd7af
Sy Rajeswaran <sy.rajeswaran25@gmail.com>
2019-06-01 14:27:13 -0400

    Error feedback for the order and checkout files

--- checkout/script.js
+++ checkout/script.js
@@ -4,3 +4,17 @@ function Home(){
 function back(){
     location.replace("http://127.0.0.1:5500/order/OrderList.html")
 }
+
+function Feedback(){
+    if(document.forms["payment"]["street_name"].value == "" || document.forms["payment"]["postal_code"].value == "" || 
+    document.forms["payment"]["card_holder"].value == "" || document.forms["payment"]["card_number"].value == "" || 
+    document.forms["payment"]["expiry_date"].value == "" || document.forms["payment"]["cvc"].value == "" ){
+        alert("Missing Input!!")
+    }
+    else{
+        alert("Order is Confirmed")
+        location.replace("http://127.0.0.1:5500/welcome/")
+
+    }
+    
+}
--- order/script.js
+++ order/script.js
@@ -1,5 +1,13 @@
 function changePage() {
-    location.replace("http://127.0.0.1:5500/checkout/")
+    let total="total";
+    var currentTotal =parseFloat(document.querySelector("#"+total).firstChild.data);
+    if(currentTotal == 0.00){
+        alert("Your Order is Empty")
+    }
+    else{
+        location.replace("http://127.0.0.1:5500/checkout/")
+    }
+    
     
 }
 
