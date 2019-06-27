c44dc2976807c5d4155049ff8842fad78c176e6d
Maxim Melkonian <mmelk057@uottawa.ca>
2019-06-27 19:26:39 -0400

    #12: Create navigation bar component for app

--- react-src/BiteMe/index.js
+++ react-src/BiteMe/index.js
@@ -4,6 +4,19 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import MyComponent from './components/navigation';
+import * as React from 'react';
+import {Provider as PaperProvider} from 'react-native-paper';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main(){
+    return (
+        <PaperProvider>
+            <MyComponent></MyComponent>
+            <App></App>
+        </PaperProvider>
+    )
+}
+
+
+AppRegistry.registerComponent(appName, () => Main);
